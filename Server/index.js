75e8bcd7492fe5105acbbf209e09838cf1d9b5b2
Shubham Singh <shubhamsinghcsk@gmail.com>
2023-01-02 10:41:38 +0530

    Full stack MERN project
    Full stack MERN project

---
+++ Server/index.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import postRoutes from './routes/posts.js';
+import userRoutes from './routes/users.js'; //
+import dotenv from 'dotenv';
+
+const app = express();
+dotenv.config();
+
+app.use(bodyParser.json({limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
+app.use(cors());
+
+app.use('/posts',postRoutes);
+app.use('/user',userRoutes);
+
+// const CONNECTION_URL = 'mongodb+srv://memoriesproject:438402hu@cluster0.l5hs5ts.mongodb.net/?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(process.env.CONNECTION_URL, {useNewUrlParser:true, useUnifiedTopology:true})
+    .then(()=> app.listen(PORT, ()=> console.log(`server running on ${PORT}`)))
+    .catch((error)=>console.log(error.message));             
+
+
+
